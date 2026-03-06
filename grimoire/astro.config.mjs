bb582ac53509b04f0facd61b1b0fbee164a8e66e
wct9667 <wct9667@rit.edu>
2026-03-05 22:31:04 -0500

    Update astro.config.mjs

--- grimoire/astro.config.mjs
+++ grimoire/astro.config.mjs
@@ -4,9 +4,10 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
+const isProd = process.env.NODE_ENV === 'production'
+
 export default defineConfig({
-	site: 'https://github.com/wct9667.github.io/william-tallarico.io',
-  	base: '/william-tallarico.io/',
-  	integrations: [mdx(), sitemap()],
-});
+  site: 'https://wct9667.github.io/william-tallarico.io/',
+  base: isProd ? '/william-tallarico.io/' : '/',
+  integrations: [mdx(), sitemap()],
+})
