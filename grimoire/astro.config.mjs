34ebf6824acb293a693180b8079895d71650038a
wct9667 <wct9667@rit.edu>
2026-03-05 21:53:56 -0500

    Update astro.config.mjs

--- grimoire/astro.config.mjs
+++ grimoire/astro.config.mjs
@@ -6,6 +6,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://github.com/wct9667/william-tallarico.io',
+	  base: '/grimoire/',
 	integrations: [mdx(), sitemap()],
 });
