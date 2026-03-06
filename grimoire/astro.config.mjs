9715ca20bb40ac5481b17a34c6fc0675864de88c
wct9667 <wct9667@rit.edu>
2026-03-05 22:17:04 -0500

    Trying to fix paths

--- grimoire/astro.config.mjs
+++ grimoire/astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://github.com/wct9667/william-tallarico.io',
+	site: 'https://github.com/wct9667.githu.io/william-tallarico.io',
   	base: '/william-tallarico.io/',
   	integrations: [mdx(), sitemap()],
 });
