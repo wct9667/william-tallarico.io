ad6a6d94016f2cead394f9095ef572553440049a
wct9667 <wct9667@rit.edu>
2026-03-06 11:46:16 -0500

    Setup Project Sections

--- grimoire/src/content.config.ts
+++ grimoire/src/content.config.ts
@@ -16,4 +16,20 @@ const blog = defineCollection({
 		}),
 });
 
-export const collections = { blog };
+const project = defineCollection({
+	// Load Markdown and MDX files in the `src/content/blog/` directory.
+	loader: glob({ base: './src/content/project', pattern: '**/*.{md,mdx}' }),
+	// Type-check frontmatter using a schema
+	schema: ({ image }) =>
+		z.object({
+			title: z.string(),
+			description: z.string(),
+			// Transform string to Date object
+			pubDate: z.coerce.date(),
+			updatedDate: z.coerce.date().optional(),
+			heroImage: image().optional(),
+		}),
+	//TODO Update above to be project specific
+});
+
+export const collections = { blog, project };
