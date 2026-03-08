170ce9def87e783d9aa753186884287c0e26e67e
wct9667 <wct9667@rit.edu>
2026-03-07 21:30:02 -0500

    Tweaks to Header

--- grimoire/astro.config.mjs
+++ grimoire/astro.config.mjs
@@ -1,7 +1,8 @@
-
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
 
 const isProd = process.env.NODE_ENV === 'production'
 
@@ -9,4 +10,8 @@ export default defineConfig({
   site: 'https://wct9667.github.io/william-tallarico.io/',
   base: isProd ? '/william-tallarico.io/' : '/',
   integrations: [mdx(), sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 })
