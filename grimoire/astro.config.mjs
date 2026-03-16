132413379627c29058c4640d3f03464e941a3857
wct9667 <wct9667@rit.edu>
2026-03-16 00:40:44 -0400

    trying to fix it :(

--- grimoire/astro.config.mjs
+++ grimoire/astro.config.mjs
@@ -4,11 +4,9 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
-const isProd = process.env.NODE_ENV === 'production'
-
 export default defineConfig({
   site: 'https://wct9667.github.io',
-  base: isProd ? '/' : '/',
+  base:'/',
   integrations: [mdx(), sitemap()],
 
   vite: {
