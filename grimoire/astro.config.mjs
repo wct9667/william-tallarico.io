5fd2639185ad8f52896e3dcbbdd5f142330ca9cd
wct9667 <wct9667@rit.edu>
2026-03-16 00:29:52 -0400

    Update astro.config.mjs

--- grimoire/astro.config.mjs
+++ grimoire/astro.config.mjs
@@ -7,8 +7,8 @@ import tailwindcss from '@tailwindcss/vite';
 const isProd = process.env.NODE_ENV === 'production'
 
 export default defineConfig({
-  site: 'https://wct9667.github.io',
-  base: isProd ? '/william.tallarico.io/' : '/',
+  site: 'https://wct9667.github.io/william-tallarico.io/',
+  base: isProd ? '/william-tallarico.io/' : '/',
   integrations: [mdx(), sitemap()],
 
   vite: {
